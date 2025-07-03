46007ed532277e7691cbbbcbeaf205b29c14e8f0
MinhNguyen2k4 <tanminh.office183@gmail.com>
2025-07-03 20:15:17 +0700

    add TaskList

--- my-app/src/components/TaskList/TaskList.tsx
+++ my-app/src/components/TaskList/TaskList.tsx
@@ -1,35 +1,42 @@
+import type { Todo } from "../../@types/todo.type";
 import styles from "./tasklist.module.scss";
 
 interface TaskListProps {
   doneTaskList?: boolean;
+  todos: Todo[];
+  handleDoneTodo: (id: string, done: boolean) => void;
 }
 
 export default function TaskList(props: TaskListProps) {
-  const { doneTaskList } = props;
+  const { doneTaskList, todos, handleDoneTodo } = props;
+
   return (
     <div className="mb-2">
       <h2 className={styles.title}>
         {doneTaskList ? "Hoàn thành" : "Chưa hoàn thành"}
       </h2>
       <div className={styles.tasks}>
-        <div className={styles.task}>
-          <input type="checkbox" className={styles.taskCheckbox} />
-          <span className={styles.taskName}>Hoc bai</span>
-          <div className={styles.taskActions}>
-            <button className={styles.taskBtn}>🖊️</button>
-            <button className={styles.taskBtn}>🗑️</button>
+        {todos.map((todo) => (
+          <div className={styles.task} key={todo.id}>
+            <input
+              type="checkbox"
+              className={styles.taskCheckbox}
+              checked={todo.done}
+              onChange={(event) =>
+                handleDoneTodo(todo.id, event.target.checked)
+              }
+            />
+            <span
+              className={`${styles.taskName} ${todo.done ? styles.taskNameDone : ""}`}
+            >
+              {todo.name}
+            </span>
+            <div className={styles.taskActions}>
+              <button className={styles.taskBtn}>🖊️</button>
+              <button className={styles.taskBtn}>🗑️</button>
+            </div>
           </div>
-        </div>
-        <div className={styles.task}>
-          <input type="checkbox" className={styles.taskCheckbox} />
-          <span className={`${styles.taskName} ${styles.taskNameDone}`}>
-            Hoc bai
-          </span>
-          <div className={styles.taskActions}>
-            <button className={styles.taskBtn}>🖊️</button>
-            <button className={styles.taskBtn}>🗑️</button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
--- my-app/src/components/TodoList/TodoList.tsx
+++ my-app/src/components/TodoList/TodoList.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import TaskInput from "../TaskInput";
 import TaskList from "../TaskList";
+import type { Todo } from "../../@types/todo.type";
 import styles from "./todolist.module.scss";
 
 export default function TodoList() {
+  const [todos, settodos] = useState<Todo[]>([]);
+
+  const doneTodos = todos.filter((todo) => todo.done);
+  const notdoneTodos = todos.filter((todo) => !todo.done);
+
+  const addTodo = (name: string) => {
+    const todo: Todo = {
+      name,
+      done: false,
+      id: new Date().toISOString(),
+    };
+    settodos((prev) => [...prev, todo]);
+  };
+  const handleDoneTodo = (id: string, done: boolean) => {
+    settodos((prev) => {
+      return prev.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, done };
+        }
+        return todo;
+      });
+    });
+  };
+  console.log(todos);
   return (
     <div className={styles.todoList}>
       <div className={styles.todoListContainer}>
-        <TaskInput />
-        <TaskList />
-        <TaskList doneTaskList />
+        <TaskInput addTodo={addTodo} />
+        <TaskList todos={notdoneTodos} handleDoneTodo={handleDoneTodo} />
+        <TaskList doneTaskList todos={doneTodos} handleDoneTodo={handleDoneTodo}/>
       </div>
     </div>
   );
